130abe94c635e0b3ab64730361fa36348fc7c5ec
Vinicius Santos <viniciushacrinha1@gmail.com>
2022-02-08 13:10:43 -0300

    Developed get person by ID

--- src/main/java/one/digitalino/personapi/controller/PersonController.java
+++ src/main/java/one/digitalino/personapi/controller/PersonController.java
@@ -4,18 +4,21 @@ import lombok.AllArgsConstructor;
 import one.digitalino.personapi.dto.request.PersonDTO;
 import one.digitalino.personapi.dto.response.MessageResponseDTO;
 import one.digitalino.personapi.entity.Person;
+import one.digitalino.personapi.exception.PersonNotFoundException;
 import one.digitalino.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
     private final PersonService personService;
+    private PersonService personService1;
 
     @Autowired
     public PersonController(PersonService personService){
@@ -27,5 +30,15 @@ public class PersonController {
     @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO){
         return personService.createPerson(personDTO);
+    }
+
+    @GetMapping
+    public List<PersonDTO> listAll(){
+        return personService.listAll();
+    }
+
+    @GetMapping("/{id}")
+    public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
+        return personService.findById(id);
     }
 }
--- src/main/java/one/digitalino/personapi/service/PersonService.java
+++ src/main/java/one/digitalino/personapi/service/PersonService.java
@@ -4,10 +4,15 @@ import lombok.AllArgsConstructor;
 import one.digitalino.personapi.dto.request.PersonDTO;
 import one.digitalino.personapi.dto.response.MessageResponseDTO;
 import one.digitalino.personapi.entity.Person;
+import one.digitalino.personapi.exception.PersonNotFoundException;
 import one.digitalino.personapi.mapper.PersonMapper;
 import one.digitalino.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor(onConstructor = @__(@Autowired))
@@ -27,10 +32,24 @@ public class PersonService {
         return messageResponse;
     }
 
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+    public PersonDTO findById(Long id) throws PersonNotFoundException {
+     Person person = personRepository.findById(id)
+              .orElseThrow(() -> new PersonNotFoundException(id));
+        return personMapper.toDTO(person);
+    }
+
     private MessageResponseDTO createMessageResponse(String s, Long id2) {
         return MessageResponseDTO.builder()
                 .message(s + id2)
                 .build();
     }
+
+
 
 }
